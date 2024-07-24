ca9fc7f346baa6c7ec34b7949611e3ab00954872
LK <nsanzby37@gmail.com>
2024-07-24 08:59:44 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,72 @@
+//LES VARIABLES EN JS
+
+
+
+//LES OPERATIONS ENTRE VARIABLE
+
+//LES OPERATIONS DE COMPARAISON
+
+
+//CONDITION
+
+
+//OPERATION LOGIQUE
+ 
+
+
+//SWITCH (teste que l egalité)
+
+let month ="janvier"
+switch (month) {
+    case "janvier":
+        alert("belier")
+        
+        break;
+    
+    case "fevrier" :
+         alert("capricorne")
+     
+     break;
+    case "mars":
+
+      alert("sagitaire")
+     break;
+
+    case "avril":
+
+     alert("jumaux")
+     break;
+    case "mai":
+       alert("vierge")
+     
+     break;
+    case "juin":
+      alert("balance")
+     
+     break;
+    case "juillet":
+      alert("cancer")
+     
+     break;
+ 
+    case "aout":
+      alert("lion")
+     
+     break;
+    case "semtembre":
+      alert("tauraeu")
+     
+     break;
+    case "octobre":
+       alert("poisson")
+     
+     break;
+    case "novembre":
+      alert("scorpion")
+     
+     break;
+
+
+    default:
+        break;
+}
